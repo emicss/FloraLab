4acb15e24d9a77b4659955f81118ce2a592c3d93
emicss <emi251217@gmail.com>
2024-01-25 12:35:46 -0600

    .

---
+++ js/index.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.about', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+
+sr.reveal('.contact', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+});
+
+sr.reveal('.products', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+    delay: 1000
+});
+
+sr.reveal('.icons-container', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px',
+
+});
+
+sr.reveal('.trabajo', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px',
+});
+
+sr.reveal('.ubi', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+
+sr.reveal('.home', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+});
+
+sr.reveal('.content', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+});
+
+sr.reveal('.imgcontainer', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+});
---
+++ js/pr.js
@@ -0,0 +1,7 @@
+$('#btn').click(function(){
+    Swal.fire({
+        title: "Tus datos se an enviado correctamente",
+        text: "Nos pondremos en contacto contigo lo antes posible",
+        icon: "success"
+      });
+})
